57f1fe977f6bf94cf6020deca9a9788018fa4574
Felipe A. A. Rios <felipe.rios12@outlook.com>
2023-01-22 20:53:49 -0300

    aplicação web completa

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import { prisma } from "./lib/prisma";
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
+
+const app = Fastify();
+
+app.register(cors);
+app.register(appRoutes);
+app.listen({
+  port: 3333,
+});
